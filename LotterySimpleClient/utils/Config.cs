74c0144fe32521b4d33c7c3848bfe0712e5d06f2
Francesco Basco <fbasco81@gmail.com>
2021-02-15 22:50:16 +0100

    wip

--- LotterySimpleClient/utils/Config.cs
+++ LotterySimpleClient/utils/Config.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 namespace LotterySimpleClient{
     class Config
     {
-        public static string ApiUrl = "https://lottery-web-api.azurewebsites.net/WeatherForecast";
+        public static string ApiUrl = "https://lottery-web-api.azurewebsites.net/LuckyNumbers";
         public static int MaxDegreeOfParallelism = 10;
     }
 }
